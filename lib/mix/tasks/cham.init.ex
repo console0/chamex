f29dc1f0e75af44634353a119b47ebc238859392
Marc Slagle <console0@gmail.com>
2022-08-20 12:15:02 -0400

    Actually its all \

--- lib/mix/tasks/cham.init.ex
+++ lib/mix/tasks/cham.init.ex
@@ -32,14 +32,14 @@ defmodule Mix.Tasks.Cham.Init do
       dbname := #{app_name}
 
       help: ## Shows this help.
-        @IFS=$$'\n' ; \\
+        @IFS=$$'\\n' ; \\
         help_lines=(`fgrep -h "##" $(MAKEFILE_LIST) | fgrep -v fgrep | sed -e 's/\\$$//'`); \\
         for help_line in $${help_lines[@]}; do \\
           IFS=$$'#' ; \\
           help_split=($$help_line) ; \\
           help_command=`echo $${help_split[0]} | sed -e 's/^ *//' -e 's/ *$$//'` ; \\
           help_info=`echo $${help_split[2]} | sed -e 's/^ *//' -e 's/ *$$//'` ; \\
-          printf "%-30s %s\n" $$help_command $$help_info ; \\
+          printf "%-30s %s\\n" $$help_command $$help_info ; \\
         done
 
       build: ## (Re)build the containers
