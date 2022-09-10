28798507265e9f5df3a0cdf4fb6f3421eea3a673
Marc Slagle <console0@gmail.com>
2022-09-10 10:31:34 -0400

    cleanup

--- lib/mix/tasks/cham.init.ex
+++ lib/mix/tasks/cham.init.ex
@@ -69,13 +69,11 @@ defmodule Mix.Tasks.Cham.Init do
   end
 
   def write_class_plug(otp_app) do
-    this_app = Mix.Cham.otp_app()
     web_name = Mix.Cham.web_name(otp_app)
     plug_path = Mix.Cham.web_path(otp_app, Path.join(["plugs"]))
     plug_file = Mix.Cham.web_path(otp_app, Path.join(["plugs", "require_class.ex"]))
 
     with :ok <- File.mkdir_p(plug_path) do
-      IO.inspect "about to write"
       File.write(
         plug_file,
         """
